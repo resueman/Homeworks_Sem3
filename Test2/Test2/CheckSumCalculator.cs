bdeed3cf121a462bdac18ae97a5d55b23d42a21f
resueman <44446084+resueman@users.noreply.github.com>
2020-12-08 11:17:16 +0300

    added

---
+++ Test2/Test2/CheckSumCalculator.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test2
+{
+    /// <summary>
+    /// Contains methods to calculate check sum of file or directory using hash algorithm
+    /// </summary>
+    public class CheckSumCalculator
+    {
+        private readonly HashAlgorithm hashAlgorithm; 
+
+        /// <summary>
+        /// Creates check sum calculator with specified hash algorithm
+        /// </summary>
+        /// <param name="hashAlgorithm">Algorithm using in check sum calculation</param>
+        public CheckSumCalculator(HashAlgorithm hashAlgorithm)
+        {
+            this.hashAlgorithm = hashAlgorithm;
+        }
+
+        /// <summary>
+        /// Computes check sum of directory or file using hash algorithm
+        /// </summary>
+        /// <param name="path">Path to directory or file</param>
+        /// <returns>Check sum of directory or file</returns>
+        public async Task<byte[]> Calculate(string path)
+        {
+            if (File.Exists(path))
+            {
+                return await CalculateFileCheckSum(path);
+            }
+            else if (Directory.Exists(path))
+            {
+                return await CalculateDirectoryCheckSum(path);
+            }
+            throw new IncorrectPathException("No such directory or file");
+        }
+
+        private async Task<byte[]> CalculateFileCheckSum(string path)
+        {
+            using var streamReader = new StreamReader(path);
+            var fileContent = await streamReader.ReadToEndAsync();
+            return GetHash(Encoding.UTF8.GetBytes(fileContent));
+        }
+
+        private async Task<byte[]> CalculateDirectoryCheckSum(string path)
+        {
+            var files = Directory.GetFiles(path);
+            Array.Sort(files);
+
+            var directories = Directory.GetDirectories(path);
+            Array.Sort(directories);
+
+            var content = Encoding.UTF8.GetBytes(Path.GetDirectoryName(path));
+            foreach (var file in files)
+            {
+                var fileCheckSum = await CalculateFileCheckSum(file);
+                content = Concat(content, fileCheckSum);
+            }
+            foreach (var directory in directories)
+            {
+                var directoryCheckSum = await CalculateDirectoryCheckSum(directory);
+                content = Concat(content, directoryCheckSum);
+            }
+            return GetHash(content);
+        }
+
+        private byte[] GetHash(byte[] input) => hashAlgorithm.ComputeHash(input);
+
+        private byte[] Concat(byte[] first, byte[] second)
+        {
+            var resultOfConcatenation = new byte[first.Length + second.Length];
+            Buffer.BlockCopy(first, 0, resultOfConcatenation, 0, first.Length);
+            Buffer.BlockCopy(second, 0, resultOfConcatenation, first.Length, second.Length);
+            return resultOfConcatenation;
+        }
+
+        /// <summary>
+        /// Changes data type of check sum
+        /// </summary>
+        /// <param name="byteCheckSum">Check sum</param>
+        /// <returns>Check sum in string format</returns>
+        public static string ByteCheckSumToStringCheckSum(byte[] byteCheckSum)
+        {
+            var hex = new StringBuilder(byteCheckSum.Length * 2);
+            foreach (byte @byte in byteCheckSum)
+            {
+                hex.AppendFormat("{0:x2}", @byte);
+            }
+            return hex.ToString();
+        }
+    }
+}
---
+++ Test2/Test2/IncorrectPathException.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Test2
+{
+    /// <summary>
+    /// Throws when there is no file or directory corresponding to specified path
+    /// </summary>
+    [Serializable]
+    internal class IncorrectPathException : Exception
+    {
+        /// <summary>
+        /// Throws when there is no file or directory corresponding to specified path
+        /// </summary>
+        public IncorrectPathException()
+        {
+        }
+
+        /// <summary>
+        /// Throws when there is no file or directory corresponding to specified path
+        /// </summary>
+        /// <param name="message">The message that describes an exception</param>
+        public IncorrectPathException(string message) 
+            : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Throws when there is no file or directory corresponding to specified path
+        /// </summary>
+        /// <param name="message">The message that describes an exception</param>
+        /// <param name="innerException">Exception that is the cause of current exception</param>
+        public IncorrectPathException(string message, Exception innerException) 
+            : base(message, innerException)
+        { 
+        }
+
+        /// <summary>
+        /// Throws when there is no file or directory corresponding to specified path
+        /// </summary>
+        /// <param name="info">All data needed to serialize and deserialize object</param>
+        /// <param name="context">Sourse or destination of a given serialized stream</param>
+        protected IncorrectPathException(SerializationInfo info, StreamingContext context) 
+            : base(info, context)
+        {
+        }
+    }
+}
